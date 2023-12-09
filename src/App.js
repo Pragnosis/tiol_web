46a91d41771ea3b6d9bb92d638bbc32af260805f
Jitendra <jitendra@pullgain.com>
2023-12-09 13:55:13 +0530

    protected routes implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/index";
 import { LandingLayout } from "./pages/landinglayout";
 import { DetailedLayout } from "./pages/detaillayout";
@@ -16,37 +16,37 @@ import { Registration } from "./pages/UserManagment/register/Registration";
 import{ ForgotPassword } from "./pages/UserManagment/ForgotPassword";
 import{ ChangePassword } from "./pages/UserManagment/ChangePassword";
 import{ SignIn } from "./pages/UserManagment/SignIn";
+import ProtectedRoute from "./utill/ProtectedRoute";
 const App = () => {
   return (
+      <Routes>
+        
+        {/* <Route path="/incometax/caselaw/sccases" element={<LandingLayout />} /> */}
+        <Route path="/:parent/:submenu1/:submenu2/:submenu3/details" element={<ProtectedRoute><DetailedLayout /></ProtectedRoute>} />
+        <Route path="/:parent/:submenu1/:submenu2/:submenu3" element={<ProtectedRoute><LandingLayout /></ProtectedRoute>} />
+        <Route path="/:parent/:submenu1/:submenu2/details" element={<ProtectedRoute><DetailedLayout /></ProtectedRoute>} />
+        <Route path="/:parent/:submenu1/:submenu2" element={<ProtectedRoute><LandingLayout /></ProtectedRoute>} />
+        <Route path="/:parent/:submenu1/details" element={<ProtectedRoute><DetailedLayout /></ProtectedRoute>} />
+        <Route path="/:parent/:submenu1" element={<ProtectedRoute><LandingLayout /></ProtectedRoute>} />
+        <Route path="/:parent/details" element={<ProtectedRoute><DetailedLayout /></ProtectedRoute>} />
+        <Route path="/:parent/news" element={<ProtectedRoute><NewsLandingLayout /></ProtectedRoute>} />
+        <Route path="/:parent" element={<ProtectedRoute><LandingLayout /></ProtectedRoute>} />
+        <Route path="/bookmark" element={<ProtectedRoute><CaselawBookMark /></ProtectedRoute>} />
+        {/* <Route path="/notification" element={<ProtectedRoute><NotificationLandingLayout /></ProtectedRoute>} /> */}
+        <Route path="/notification/details" element={<ProtectedRoute><NotificationDetailedLayout /></ProtectedRoute>} />
+        <Route path="/newsdetails" element={<ProtectedRoute><NewsDetailedLayout /></ProtectedRoute>} />
+        <Route path="/CommonSearch" element={<ProtectedRoute><CommonSearch /></ProtectedRoute>} />
+        <Route path="/tiol-web" element={<Home />} />
+        <Route path="/incometax/notifications" element={<ProtectedRoute><LandingLayout /></ProtectedRoute>} />
+        <Route path="/search/column" element={<ProtectedRoute><ColumnSearch /></ProtectedRoute>} />
+        <Route path="/search/circulars" element={<ProtectedRoute><NotificaionsCirculars /></ProtectedRoute>} />
+        <Route path="/gst/sgst" element={<ProtectedRoute><SGSTPage /></ProtectedRoute>} />
 
-    <Routes>
-
-      {/* <Route path="/incometax/caselaw/sccases" element={<LandingLayout />} /> */}
-      <Route path="/:parent/:submenu1/:submenu2/:submenu3/details" element={<DetailedLayout />} />
-      <Route path="/:parent/:submenu1/:submenu2/:submenu3" element={<LandingLayout />} />
-      <Route path="/:parent/:submenu1/:submenu2/details" element={<DetailedLayout />} />
-      <Route path="/:parent/:submenu1/:submenu2" element={<LandingLayout />} />
-      <Route path="/:parent/:submenu1/details" element={<DetailedLayout />} />
-      <Route path="/:parent/:submenu1" element={<LandingLayout />} />
-      <Route path="/:parent/details" element={<DetailedLayout />} />
-      <Route path="/:parent/news" element={<NewsLandingLayout />} />
-      <Route path="/:parent" element={<LandingLayout />} />
-      <Route path="/bookmark" element={<CaselawBookMark />} />
-      {/* <Route path="/notification" element={<NotificationLandingLayout />} /> */}
-      <Route path="/notification/details" element={<NotificationDetailedLayout />} />
-      <Route path="/newsdetails" element={<NewsDetailedLayout />} />
-      <Route path="/CommonSearch" element={<CommonSearch />} />
-      <Route path="/tiol-web" element={<Home />} />
-      <Route path="/incometax/notifications" element={<LandingLayout />} />
-      <Route path="/search/column" element={<ColumnSearch />} />
-      <Route path="/search/circulars" element={<NotificaionsCirculars />} />
-      <Route path="/gst/sgst" element={<SGSTPage />} />
-      <Route path="/registration" element={<Registration />} />
-      <Route path="/forgotpassword" element={<ForgotPassword />} />
-      <Route path="/changepassword" element={<ChangePassword />} />
-      <Route path="/signin" element={<SignIn />} />
-    </Routes>
-
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/changepassword" element={<ChangePassword />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
   );
 }
 
